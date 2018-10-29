945c0a4043f00caa156bf3ce456df4a097031c30
Sergio Jair Cerezo Vallejo <scerezo@tiempodevelopment.com>
2018-10-29 10:34:31 -0600

    Initial Commit

---
+++ Starboos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starboos
+//
+//  Created by Sergio Jair Cerezo Vallejo on 10/29/18.
+//  Copyright © 2018 Tiempo Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
